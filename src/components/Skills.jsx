c884626902773fb730eaed1a43ad44ce1d49ab0f
deelaurence <laurie143pelumi@gmail.com>
2022-09-23 13:36:40 +0100

    third deployment

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -19,7 +19,7 @@ const Skills = () => {
       </div>
       <div className="flex justify-center  mt-3 flex-wrap gap-4">
         <div
-          className="bg-darkShade overflow-hidden rounded-[4px] py-9 pl-6 text-lightShade h-[160px] w-[160px]"
+          className="bg-darkShade overflow-hidden rounded-[4px] pt-7 pl-6 text-lightShade h-[160px] w-[160px]"
         >
           <img className="mt-3 w-[30px]" src={mobile} alt="" />
           <p className="pt-4 text-xs font-semibold overflow-visible">
@@ -27,7 +27,7 @@ const Skills = () => {
           </p>
         </div>
         <div
-          className="bg-darkShade overflow-hidden rounded-[4px] py-9 pl-8 text-lightShade h-[160px] w-[160px]"
+          className="bg-darkShade overflow-hidden rounded-[4px] py-9 pl-6 text-lightShade h-[160px] w-[160px]"
         >
           <img
             className="w-[30px] mt-3"
@@ -35,11 +35,11 @@ const Skills = () => {
             alt=""
           />
           <p className="pt-4 text-xs font-semibold overflow-visible">
-            User Experience Design
+            User Experience <br /> Design
           </p>
         </div>
         <div
-          className="bg-darkShade overflow-hidden rounded-[4px] py-9 pl-8 text-lightShade h-[160px] w-[160px]"
+          className="bg-darkShade overflow-hidden rounded-[4px] py-9 pl-6 text-lightShade h-[160px] w-[160px]"
         >
           <img
             className="w-[30px] mt-3"
@@ -51,7 +51,7 @@ const Skills = () => {
           </p>
         </div>
         <div
-          className="bg-darkShade overflow-hidden rounded-[4px] py-9 pl-8 text-lightShade h-[160px] w-[160px]"
+          className="bg-darkShade overflow-hidden rounded-[4px] py-9 pl-6 text-lightShade h-[160px] w-[160px]"
         >
           <img className="w-[30px] mt-3 " src={quill} alt="" />
           <p className="pt-4 text-xs font-semibold leading-none overflow-visible">
@@ -59,7 +59,7 @@ const Skills = () => {
           </p>
         </div>
         <div
-          className="bg-darkShade rounded-[4px] py-9 pl-8 text-lightShade h-[160px] w-[160px]"
+          className="bg-darkShade rounded-[4px] py-9 pl-6 text-lightShade h-[160px] w-[160px]"
         >
           <img className="w-[30px] mt-3" src={arrowGuide} alt="" />
           <p className="pt-4 text-xs font-semibold leading-none overflow-visible">
